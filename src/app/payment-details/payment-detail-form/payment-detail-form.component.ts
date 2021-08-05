6394671b0f630a7ab44250db9adf405255f48f3e
crispinandrade <crispin.andrade95@gmail.com>
2021-08-05 18:13:50 +1000

    Initial structure

---
+++ src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-detail-form',
+  template: `
+    <p>
+      payment-detail-form works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PaymentDetailFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/payment-details/payment-details.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-details',
+  template: `
+    <p>
+      payment-details works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PaymentDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
